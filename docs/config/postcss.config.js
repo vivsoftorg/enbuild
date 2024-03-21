1e9c48c11e0938081c708c8facc62e2a282ade5d
Juned Memon <junaid18183@gmail.com>
2024-03-21 17:58:04 +0530

    Added assets/cover_old.png

--- docs/config/postcss.config.js
+++ docs/config/postcss.config.js
@@ -57,12 +57,10 @@ module.exports = {
         'page-item',
         'page-link',
         ...whitelister([
-          './assets/scss/**/*.css',
           './assets/scss/**/*.scss',
-          './node_modules/katex/dist/katex.css',
           './node_modules/@hyas/doks-core/assets/scss/components/_code.scss',
           './node_modules/@hyas/doks-core/assets/scss/components/_expressive-code.scss',
-          './node_modules/@hyas/doks-core/assets/scss/common/_syntax.scss'
+          './node_modules/@hyas/doks-core/assets/scss/common/_syntax.scss',
         ]),
       ],
     }),
