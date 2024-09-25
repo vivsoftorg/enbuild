20957339bb5a062dfec7c281e15916b52f037e70
Juned Memon <junaid18183@gmail.com>
2024-09-25 14:24:04 +0530

    Added demo_scripts/create_enbuild_demo.sh

--- cli/demo_scripts/create_enbuild_demo.sh
+++ cli/demo_scripts/create_enbuild_demo.sh
@@ -4,57 +4,44 @@ set -eu
 
 CLUSTER_NAME=${1:-enbuild}
 DEBUG=${2:-false}
+VALUES_FILE="/tmp/values.yaml"
 
-case $2 in
-  true)
+HELM_DEBUG=""
+
+if [ "$DEBUG" == "true" ]; then
     set -x
     HELM_DEBUG="--debug"
-  ;;
-
-  *)
-    HELM_DEBUG=""
-  ;;
-esac
+fi
 
 POWERSHELL_CMD='powershell.exe'
 
-
-get_cluster_values() {
-  clusterId=$1
-}
-
 get_or_create_cluster() {
   clusterName=$1
-  clusterExist=$(k3d cluster list -o json | jq '.[] | select(.name=="'"$clusterName"'") | .name')
-  if [ "$clusterExist" = "" ]
-  then
+  if ! k3d cluster list -o json | jq -e '.[] | select(.name=="'"$clusterName"'")' >/dev/null; then
     k3d cluster create "$clusterName" \
-    --image 'docker.io/rancher/k3s:v1.28.9-k3s1' \
-    --subnet '172.42.0.0/16' \
-    --k3s-arg "--node-ip=172.42.0.3@server:0" \
-    --k3s-arg "--disable=traefik@server:*" \
-    --registry-create enbuild-registry.lan \
-    --port "80:80@loadbalancer" --port "443:443@loadbalancer"
+      --image 'docker.io/rancher/k3s:v1.28.9-k3s1' \
+      --subnet '172.42.0.0/16' \
+      --k3s-arg "--node-ip=172.42.0.3@server:0" \
+      --k3s-arg "--disable=traefik@server:*" \
+      --registry-create enbuild-registry.lan \
+      --port "80:80@loadbalancer" --port "443:443@loadbalancer"
   else
     k3d cluster start "$clusterName"
   fi
 }
 
 install_or_upgrade_helm_charts() {
-  releaseExist=$(helm list -n enbuild -o json | jq '.[] | select(.name=="enbuild") | .name')
-  if [ "$releaseExist" = "" ]
-  then
+  if ! helm list -n enbuild -o json | jq -e '.[] | select(.name=="enbuild")' >/dev/null; then
     set -x
-    helm upgrade --install --create-namespace ${HELM_DEBUG} --timeout=15m -n enbuild -f values.yaml --atomic \
+    helm upgrade --install --create-namespace ${HELM_DEBUG} --timeout=15m -n enbuild -f $VALUES_FILE --atomic \
       --set global.image.cert-pullPolicy=IfNotPresent \
       enbuild vivsoft/enbuild
   fi
 
   for i in $(seq 1 3); do
-    set -x
-    helm upgrade --install --create-namespace ${HELM_DEBUG} --timeout=15m -n enbuild -f values.yaml --wait --atomic enbuild vivsoft/enbuild && break
+    helm upgrade --install --create-namespace ${HELM_DEBUG} --timeout=15m -n enbuild -f $VALUES_FILE --wait --atomic enbuild vivsoft/enbuild && break
     set +x
-    echo "Install failed. Retrying in 10 seconds. To let the cluster initialize"
+    echo "Install failed. Retrying in 10 seconds."
     sleep 10
   done
 
@@ -62,114 +49,64 @@ install_or_upgrade_helm_charts() {
 }
 
 setup_network() {
-  if [ "$(uname -s)" = 'Darwin' ]; then
-    # MacOs
-    set -x
-    sudo ifconfig lo0 alias 172.42.0.3/32 up || true
-  elif grep -qi microsoft /proc/version; then
-    # Wsl
-    set -x
-    sudo ip addr add 172.42.0.3/32 dev lo || true
-    ${POWERSHELL_CMD} -Command "Start-Process powershell -Verb RunAs -ArgumentList \"netsh interface ipv4 add address name='Loopback Pseudo-Interface 1' address=172.42.0.3 mask=255.255.255.255 skipassource=true\""
-  fi
-  set +x
+  case "$(uname -s)" in
+    Darwin)
+      sudo ifconfig lo0 alias 172.42.0.3/32 up || true
+      ;;
+    Linux)
+      if grep -qi microsoft /proc/version; then
+        sudo ip addr add 172.42.0.3/32 dev lo || true
+        ${POWERSHELL_CMD} -Command "Start-Process powershell -Verb RunAs -ArgumentList \"netsh interface ipv4 add address name='Loopback Pseudo-Interface 1' address=172.42.0.3 mask=255.255.255.255 skipassource=true\""
+      fi
+      ;;
+  esac
 }
 
 try_install_missing_deps() {
-  if which sudo >/dev/null; then
+  if command -v sudo >/dev/null; then
     SUDO="sudo"
   else
     SUDO=""
   fi
 
-  if which apt-get >/dev/null; then
+  if command -v apt-get >/dev/null; then
     echo "Installing dependencies with apt"
     ${SUDO} apt-get update && ${SUDO} apt-get install -y jq grep sed curl iproute2
-  elif which yum >/dev/null; then
+  elif command -v yum >/dev/null; then
     echo "Installing dependencies with yum"
     ${SUDO} yum update -y && ${SUDO} yum install -y jq grep sed curl iproute
-  elif which pacman >/dev/null; then
+  elif command -v pacman >/dev/null; then
     echo "Installing dependencies with pacman"
     ${SUDO} pacman -Sy && ${SUDO} pacman --noconfirm -S jq grep curl sed iproute
-  elif which brew >/dev/null; then
+  elif command -v brew >/dev/null; then
     echo "Installing dependencies with brew"
     brew update && brew install jq grep curl
   else
-    echo "Cannot detect your package manager. Please install the following command 'jq grep curl sed iproute2'"
+    echo "Cannot detect your package manager. Please install the following commands: jq grep curl sed iproute2"
     exit 1
   fi
 }
 
 install_deps() {
-  if which jq >/dev/null; then
-     echo "jq already installed"
-  else
-    try_install_missing_deps
-  fi
-
-  if which grep >/dev/null; then
-     echo "grep already installed"
-  else
-    try_install_missing_deps
-  fi
-
-  if which sed >/dev/null; then
-     echo "sed already installed"
-  else
-    try_install_missing_deps
-  fi
-
-  if test -f /proc/version && grep -qi microsoft /proc/version; then
-    if which ip >/dev/null; then
-       echo "iproute already installed"
-    else
+  for dep in jq grep sed curl docker k3d helm; do
+    if ! command -v $dep >/dev/null; then
+      echo "$dep not installed, attempting to install..."
       try_install_missing_deps
+    else
+      echo "$dep already installed"
     fi
-  fi
+  done
 
-  if which curl >/dev/null; then
-     echo "curl already installed"
-  else
-    try_install_missing_deps
-  fi
-
-  if which docker >/dev/null; then
-     echo "docker already installed"
-  else
-    echo "docker command is missing. Please use your package manager to install it"
-    echo "https://docs.docker.com/engine/install/"
-    exit 1
-  fi
-
-  docker_running=$( (docker ps -q >/dev/null && echo true ) || echo false )
-  if "$docker_running" == "true"; then
-     echo "docker is running"
-  else
-    echo "Docker is not running. Please start Docker before running this command"
-    exit 1
-  fi
-
-  if which k3d >/dev/null; then
-    echo "k3d already installed"
-  else
-    echo "Installing k3d"
-    curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.6.3 bash
-  fi
-
-  if which helm >/dev/null; then
-     echo "helm already installed"
-  else
-    echo "Installing HELM"
-    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-  fi
-
-  # Wsl powershell
   if test -f /proc/version && grep -qi microsoft /proc/version; then
-    if which 'powershell.exe' >/dev/null; then
-      echo "powershell is installed"
+    if ! command -v ip >/dev/null; then
+      try_install_missing_deps
+    else
+      echo "iproute already installed"
+    fi
+    
+    if command -v 'powershell.exe' >/dev/null; then
       POWERSHELL_CMD='powershell.exe'
-    elif which '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe' >/dev/null; then
-      echo "powershell is installed"
+    elif command -v '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe' >/dev/null; then
       POWERSHELL_CMD='/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe'
     else
       echo "Cannot find powershell.exe, please be sure it is installed"
@@ -177,27 +114,27 @@ install_deps() {
     fi
   fi
 
+  if ! docker ps -q >/dev/null; then
+    echo "Docker is not running. Please start Docker before running this command"
+    exit 1
+  fi
+
   echo "All dependencies are installed"
 }
 
-# shellcheck disable=SC2046
-# shellcheck disable=SC2086
-cd "$(dirname $(realpath $0))"
+cd "$(dirname "$(realpath "$0")")"
 
 echo '""""""""""""""""""""""""""""""""""""""""""""'
 echo 'Checking and installing dependencies'
 echo '""""""""""""""""""""""""""""""""""""""""""""'
 install_deps
 
-accountId=$(get_or_create_on_premise_account)
-clusterId=$(get_or_create_demo_cluster "${accountId}" "${CLUSTER_NAME}")
-
 echo ''
 echo '""""""""""""""""""""""""""""""""""""""""""""'
 echo 'Fetching ENBUILD values to setup your cluster'
 echo '""""""""""""""""""""""""""""""""""""""""""""'
-curl -s -L https://raw.githubusercontent.com/vivsoftorg/enbuild/refs/heads/main/examples/enbuild/quick_install.yaml  >> values.yaml
-echo 'Helm values written into values.yaml'
+curl -s -L https://raw.githubusercontent.com/vivsoftorg/enbuild/refs/heads/main/examples/enbuild/quick_install.yaml > $VALUES_FILE
+echo 'Helm values written into $VALUES_FILE'
 
 
 echo ''
@@ -205,7 +142,7 @@ echo '""""""""""""""""""""""""""""""""""""""""""""'
 echo 'Installing ENBUILD helm repositories'
 echo '""""""""""""""""""""""""""""""""""""""""""""'
 helm repo add vivsoft https://vivsoftorg.github.io/enbuild
-helm repo update enbuild
+helm repo update vivsoft
 
 echo ''
 echo '""""""""""""""""""""""""""""""""""""""""""""'
@@ -219,19 +156,17 @@ echo 'Installing ENBUILD helm charts'
 echo '""""""""""""""""""""""""""""""""""""""""""""'
 install_or_upgrade_helm_charts
 
-
 echo ''
 echo '""""""""""""""""""""""""""""""""""""""""""""'
-echo 'Configure network'
+echo 'Configuring network'
 echo '""""""""""""""""""""""""""""""""""""""""""""'
 setup_network
-
 
 echo ''
 echo '""""""""""""""""""""""""""""""""""""""""""""'
 echo "ENBUILD demo cluster is now installed !!!!"
 echo "The kubeconfig is correctly set, so you can connect to it directly with kubectl or k9s from your local machine"
 echo "To delete/stop/start your cluster, use k3d cluster xxxx"
-echo ''
-echo "Go to https://console.enbuild.com to create your first environment on this cluster '${CLUSTER_NAME}'"
+echo "To access the ENBUILD dashboard, use port-forward with below command"
+echo "kubectl --namespace enbuild port-forward svc/enbuild-enbuild-ui 3000:80"
 echo '""""""""""""""""""""""""""""""""""""""""""""'
