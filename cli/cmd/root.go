fa4293f05c5462696a1b54dde4e79f46e63b6960
Juned Memon <junaid18183@gmail.com>
2025-05-15 18:16:10 +0530

    Added GET command

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -13,7 +13,7 @@ var Version = "v0.0.0" // Replace with current version number as needed.
 var rootCmd = &cobra.Command{
 	Use:   "enbuild",
 	Short: "enbuild cli",
-	Long:  `enbuild is a CLI to help generate the ENBUILD catalog templates`,
+	Long:  `enbuild is a CLI to work with ENBUILD`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Check if the version flag was passed
 		versionFlag, _ := cmd.Flags().GetBool("version")
